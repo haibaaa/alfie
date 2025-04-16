16099143b38452cbfda113aaeb4c85dd64fd1111
haiba <9700samarth@gmail.com>
2025-04-16 13:29:03 +0530

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "../components/ui/button";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main>
       <div>
-      <Button>Click me</Button>
+        <Button>Click me</Button>
       </div>
     </main>
   );
