1a1468e63d4882e67a0d2cccd24ca40dc728460a
haiba <9700samarth@gmail.com>
2025-04-16 13:31:32 +0530

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import Link from "next/link";
 import { Button } from "../components/ui/button";
 
 export default function HomePage() {
   return (
-    <main>
-      <div>
-        <Button>Click me</Button>
-      </div>
-    </main>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
